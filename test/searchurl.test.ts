3c94de50ac010a98f0b9d26c9e72cdf3f39678c6
perpil <97474956+perpil@users.noreply.github.com>
2024-05-16 14:59:53 -0700

    Initial commit

---
+++ test/searchurl.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Searchurl from '../lib/searchurl-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/searchurl-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Searchurl.SearchurlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
