a5beed9bb4df8566477c5d644760886f1e72c51e
perpil <97474956+perpil@users.noreply.github.com>
2024-05-22 12:03:30 -0700

    Initial bits

--- README.md
+++ README.md
@@ -1,14 +1,53 @@
-# Welcome to your CDK TypeScript project
+# CloudFront As a Lightweight Proxy
 
-This is a blank project for CDK development with TypeScript.
+This is the companion repo for the blog post: [Using CloudFront as a Lightweight Proxy](https://speedrun.nobackspacecrew.com/blog/2024/05/22/using-cloudfront-as-a-lightweight-proxy.html) The stack will create a Google Url Bookmark service that only uses CloudFront and S3.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Interesting files
+
+1. [`lib/searchurl-stack.ts`](lib/searchurl-stack.ts) - The CDK stack that creates the CloudFront distribution, CloudFront functions, OAC, S3 bucket and lifecycle policy to clean up the bucket.
+1. [`cloudfront-functions/czldAPIRequest.js`](cloudfront-functions/czldAPIRequest.js): The CloudFront function to rewrite the s3 object key and add search terms as metadata
+1. [`cloudfront-functions/czldAPIResponse.js`](cloudfront-functions/czldAPIResponse.js): The CloudFront function to create a json response with the resulting url
+1. [`cloudfront-functions/czldRedirect.js`](cloudfront-functions/czldRedirect.js): The CloudFront function extract the search terms from S3 metadata and redirect to Google
+1. [`cloudfront-functions/czldPost.js`](cloudfront-functions/czldPost.js): A CloudFront function unused in this stack, but that creates a presigned form to post to S3.
+
+## Setup
+
+```
+npm install
+```
+
+If you've never used the [CDK](https://aws.amazon.com/cdk/) before, run:
+
+```
+npx cdk bootstrap
+```
+
+## Deploy
+
+```
+npx cdk deploy
+```
+
+## Usage
+
+**Outputs** will print a the url for the CloudFront distribution.
+
+```
+SearchUrlStack.Url = https://dxxxxxxxxxxxx.cloudfront.net
+```
+
+Enter your search terms and click `Persist`. The resulting url will load a file from S3 and redirect to Google with the search terms you originally entered.
+
+## Tearing it down
+
+```
+npx cdk destroy
+```
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npx cdk deploy` deploy this stack to your default AWS account/region
+- `npx cdk diff` compare deployed stack with current state
+- `npx cdk synth` emits the synthesized CloudFormation template
--- lib/searchurl-stack.ts
+++ lib/searchurl-stack.ts
@@ -1,16 +1,179 @@
 import * as cdk from 'aws-cdk-lib';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Reference } from 'aws-cdk-lib';
+import { S3OriginProps, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { CfnOriginAccessControl } from 'aws-cdk-lib/aws-cloudfront';
+import { Source, BucketDeployment } from 'aws-cdk-lib/aws-s3-deployment';
 
-export class SearchurlStack extends cdk.Stack {
+// To use OAC instead of OAI, we need to patch the S3Origin class
+// https://github.com/aws/aws-cdk/issues/21771#issuecomment-2081710100
+
+type S3OriginWithOACPatchProps = S3OriginProps & {
+  originAccessControlId: Reference;
+};
+
+class S3OriginWithOACPatch extends S3Origin {
+  private readonly originAccessControlId: Reference;
+
+  constructor(bucket: s3.IBucket, props: S3OriginWithOACPatchProps) {
+    super(bucket, props);
+    this.originAccessControlId = props.originAccessControlId;
+  }
+
+  public bind(
+    scope: Construct,
+    options: cloudfront.OriginBindOptions
+  ): cloudfront.OriginBindConfig {
+    const originConfig = super.bind(scope, options);
+
+    if (!originConfig.originProperty)
+      throw new Error('originProperty is required');
+
+    return {
+      ...originConfig,
+      originProperty: {
+        ...originConfig.originProperty,
+        originAccessControlId: this.originAccessControlId.toString(),
+        s3OriginConfig: {
+          ...originConfig.originProperty.s3OriginConfig,
+          originAccessIdentity: '',
+        },
+      },
+    };
+  }
+}
+
+function createCFFunction(scope: Construct, id: string) {
+  return new cloudfront.Function(scope, `${id}Fn`, {
+    code: cloudfront.FunctionCode.fromFile({
+      filePath: `cloudfront-functions/${id}.js`,
+    }),
+    runtime: cloudfront.FunctionRuntime.JS_2_0,
+    functionName: id,
+  });
+}
+
+export class SearchUrlStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const origin = new s3.Bucket(this, 'Origin', {
+      bucketName: `coldstart-zero-lootdrop-origin-${this.account}-${this.region}`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      lifecycleRules: [
+        {
+          prefix: 'r/',
+          expiration: cdk.Duration.days(1),
+          abortIncompleteMultipartUploadAfter: cdk.Duration.days(1),
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SearchurlQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new BucketDeployment(this, 'DeployWebsite', {
+      sources: [Source.asset('./website/dist')],
+      destinationBucket: origin,
+      prune: false,
+    });
+
+    const s3BucketOAC = new CfnOriginAccessControl(this, 's3-bucket-OAC', {
+      originAccessControlConfig: {
+        name: 's3-bucket-OAC',
+        originAccessControlOriginType: 's3',
+        signingBehavior: 'always',
+        signingProtocol: 'sigv4',
+      },
+    });
+
+    // By using s3.Bucket.fromBucketName instead of origin, it doesn't add the OAI policy to the bucket resource policy
+    // https://github.com/aws/aws-cdk/issues/21771#issuecomment-2094497498
+    const s3BucketOrigin = new S3OriginWithOACPatch(
+      s3.Bucket.fromBucketName(this, 'OriginBucket', origin.bucketName),
+      {
+        originAccessControlId: s3BucketOAC.getAtt('Id'),
+      }
+    );
+
+    const apiRequestFn = createCFFunction(this, 'czldAPIRequest');
+    const apiResponseFn = createCFFunction(this, 'czldAPIResponse');
+    const redirectFn = createCFFunction(this, 'czldRedirect');
+
+    let distro = new cloudfront.Distribution(this, 'distro', {
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+      defaultBehavior: {
+        origin: s3BucketOrigin,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.HTTPS_ONLY,
+        compress: true,
+      },
+      defaultRootObject: 'index.html',
+      comment: 'Coldstart Zero Loot Drop Distribution',
+      additionalBehaviors: {
+        '/r/*': {
+          origin: s3BucketOrigin,
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+          functionAssociations: [
+            {
+              function: redirectFn,
+              eventType: cloudfront.FunctionEventType.VIEWER_RESPONSE,
+            },
+          ],
+        },
+        '/api': {
+          origin: s3BucketOrigin,
+          //Allow PUT requests to the /api path
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+          originRequestPolicy:
+            cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
+          functionAssociations: [
+            {
+              function: apiRequestFn,
+              eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
+            },
+            {
+              function: apiResponseFn,
+              eventType: cloudfront.FunctionEventType.VIEWER_RESPONSE,
+            },
+          ],
+        },
+      },
+    });
+
+    // Add OAC policy to the bucket that allows read of all objects, and write of objects in the r/ prefix
+    origin.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['s3:GetObject'],
+        principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+        resources: [origin.arnForObjects('*')],
+        conditions: {
+          StringEquals: {
+            'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${distro.distributionId}`,
+          },
+        },
+      })
+    );
+
+    origin.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['s3:PutObject'],
+        principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+        resources: [origin.arnForObjects('r/*')],
+        conditions: {
+          StringEquals: {
+            'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${distro.distributionId}`,
+          },
+        },
+      })
+    );
+
+    new cdk.CfnOutput(this, 'Url', {
+      value: `https://${distro.distributionDomainName}`,
+    });
   }
 }
